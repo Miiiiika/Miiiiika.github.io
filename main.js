5b0c077dbfd0a5e193b34b32832a2ba44b48e491
Miiiiika <itunesayylmao3@gmail.com>
2023-03-29 02:31:46 -0700

    Snake Game 0.6
    Made the code better, added score.

--- main.js
+++ main.js
@@ -55,47 +55,62 @@ setInterval(funVar, 1000);
 //"positionY" corresponds to the first array position, eg. positionY at 0 will give "grid[0][x]"
 //Each position in the nested array is the x position, so Y=1,X=2 gives "grid[1][2]" = "#twoOne"
 //I messed up the labels so that it displays X first, while the grid works on Y first, but its ok.
-let grid = [["#zeroZero","#oneZero","#twoZero","#threeZero"]];
-grid [1] = ["#zeroOne","#oneOne","#twoOne","#threeOne"]
-grid [2] = ["#zeroTwo","#oneTwo","#twoTwo","#threeTwo"]
-grid [3] = ["#zeroThree","#oneThree","#twoThree","#threeThree"]
+let grid = [["#zeroThree","#oneThree","#twoThree","#threeThree"]];
+grid [1] = ["#zeroTwo","#oneTwo","#twoTwo","#threeTwo"];
+grid [2] = ["#zeroOne","#oneOne","#twoOne","#threeOne"];
+grid [3] = ["#zeroZero","#oneZero","#twoZero","#threeZero"];
 let positionX = 0;
 let positionY = 0;
-//Each function here does the following steps when the corresponding button is pressed:
-//1: checks to see if the user will move off the map. If it wont:
-//2: creates a variable called "oldY/X" stored as the current value of "positionX/Y"
-//3: increases "positionX/Y" by 1.
-//4: finds the old position of the X and changes it to an o
-//5: finds the new position of the X and changes it to an X
-let northButton = function() {
-	if (positionY >= 1) {
-		let oldY = positionY;
-		positionY -= 1;
+//Creating a score variable and a random X and Y position for the score tile. Gets a random number between 1-3 so that the S never spawns on top of the X.
+let score = 0;
+let scoreY = Math.floor((Math.random() * 3) + 1);
+let scoreX = Math.floor((Math.random() * 3) + 1);
+document.querySelector(grid[scoreY][scoreX]).innerHTML = "S";
+
+/*
+moveFunction moves the X and scores if applicable.
+0: Take arguments from the buttons for directionX/Y. For example, north button means 0x, +1y, and so gives "moveFunction(0, 1)".
+1: Assign variables oldY/X to the current position values, assign potentialY/X to their values after moving.
+2: Check if the potential movement would bring the X out of bounds. Then, check the direction that's being moved: if directionX = 0, we know it's is moving on the Y axis, and vice-versa.
+3: modify positionX/Y by directionX/Y.
+4: Find the grid position of the old X and assign it an "o".
+5: Find the grid position of the new X and assign it an "X".
+6: If the X is on a scoring tile, increase score by 1.
+7: Assign random position to the next score tile (using a while loop ensures that the next tile is never the same tile as the current X), and then update the label for it.
+*/
+moveFunction = function(directionX, directionY) {
+	let oldY = positionY;
+	let oldX = positionX;
+	let potentialY = positionY + directionY;
+	let potentialX = positionX + directionX;
+	if (potentialY > -1 && potentialY < 4 && directionX == 0) {
+		positionY += directionY;
 		document.querySelector(grid[oldY][positionX]).innerHTML = "o";
 		document.querySelector(grid[positionY][positionX]).innerHTML = "X";
+	} else if (potentialX > -1 && potentialX < 4 && directionY == 0) {
+		positionX += directionX;
+		document.querySelector(grid[positionY][oldX]).innerHTML = "o";
+		document.querySelector(grid[positionY][positionX]).innerHTML = "X";
 	}
+	if (positionY == scoreY && positionX == scoreX) {
+		score++;
+		document.querySelector("#score").innerHTML = score;
+		while (positionY == scoreY && positionX == scoreX) {
+			scoreY = Math.floor(Math.random() * 4);
+			scoreX = Math.floor(Math.random() * 4);
+		}
+		document.querySelector(grid[scoreY][scoreX]).innerHTML = "S";
+	}
+}
+let northButton = function() {
+	moveFunction(0, 1);
 }
 let southButton = function() {
-	if (positionY <= 2) {
-		let oldY = positionY;
-		positionY += 1;
-		document.querySelector(grid[oldY][positionX]).innerHTML = "o";
-		document.querySelector(grid[positionY][positionX]).innerHTML = "X";
-	}
+	moveFunction(0, -1);
 }
 let westButton = function() {
-	if (positionX >= 1) {
-		let oldX = positionX;
-		positionX -= 1;
-		document.querySelector(grid[positionY][oldX]).innerHTML = "o";
-		document.querySelector(grid[positionY][positionX]).innerHTML = "X";
-	}
+	moveFunction(-1, 0);
 }
 let eastButton = function() {
-	if (positionX <= 2) {
-		let oldX = positionX;
-		positionX += 1;
-		document.querySelector(grid[positionY][oldX]).innerHTML = "o";
-		document.querySelector(grid[positionY][positionX]).innerHTML = "X";
-	}
+	moveFunction(1, 0);
 }
