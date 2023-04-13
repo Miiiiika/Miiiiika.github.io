2fe6a3c66439e572512a5563bd4c2fb03388858a
Miiiiika <itunesayylmao3@gmail.com>
2023-04-13 02:33:47 -0700

    UofA Announcements Box
    -Added the announcements box
    -Added shadows to classes:hover
    -Fixed classes:hover moving other classes (thanks chatGPT...)
    -Fixed mobile screen divs not taking up 100% on not-index.

--- Script/d2lScript.js
+++ Script/d2lScript.js
@@ -1,10 +1,13 @@
 console.log("JS Running");
 let helpDisplay = false;
 let toolsDisplay = false;
+let annDisplay = false; //ann = announcement
 const topButton1 = document.getElementById('topButton1');
 const topButton2 = document.getElementById('topButton2');
+const anDDButton = document.getElementById('anDDButton')
 const helpMenu = document.getElementById("helpMenu");
 const toolsMenu = document.getElementById("toolsMenu");
+const annMenu = document.getElementById("annDropDown")
 const helpButton = function () {
 	if (helpDisplay == false) {
 		document.getElementById("helpMenu").style.display = 'block';
@@ -13,9 +16,11 @@ const helpButton = function () {
 		document.getElementById("helpMenu").style.display = "none";
 		helpDisplay = false;
 	}
-	if (toolsDisplay == true) {
+	if (toolsDisplay == true || annDisplay == true) {
 		document.getElementById("toolsMenu").style.display = 'none';
 		toolsDisplay = false;
+		document.getElementById("annDropDown").style.display = "none";
+		annDisplay = false;
 	}
 }
 
@@ -27,15 +32,40 @@ const toolsButton = function () {
 		document.getElementById("toolsMenu").style.display = 'none';
 		toolsDisplay = false;
 	}
-	if (helpDisplay == true) {
+	if (helpDisplay == true || annDisplay == true) {
+		document.getElementById("helpMenu").style.display = 'none';
+		helpDisplay = false;
+		document.getElementById("annDropDown").style.display = "none";
+		annDisplay = false;
+	}
+}
+const annButton = function() {
+	if (annDisplay == false) {
+		document.getElementById("annDropDown").style.display = 'block';
+		annDisplay = true;
+	} else {
+		document.getElementById("annDropDown").style.display = "none";
+		annDisplay = false;
+	}
+	if (toolsDisplay == true || helpDisplay == true) {
+		document.getElementById("toolsMenu").style.display = 'none';
+		toolsDisplay = false;
 		document.getElementById("helpMenu").style.display = 'none';
 		helpDisplay = false;
 	}
 }
+
 addEventListener('click', function(event) {
-	if (!helpMenu.contains(event.target) && !topButton1.contains(event.target) && !topButton2.contains(event.target) && !toolsMenu.contains(event.target)) {
+	if (!helpMenu.contains(event.target) && !topButton1.contains(event.target)) {
 		helpMenu.style.display = "none";
-		toolsMenu.style.display = "none"
 		helpDisplay = false;
+	}
+	if (!topButton2.contains(event.target) && !toolsMenu.contains(event.target)) {
+		toolsMenu.style.display = "none"
+		toolsDisplay = false;
+	}
+	if (!anDDButton.contains(event.target) && !annMenu.contains(event.target)) {
+		annMenu.style.display = "none"
+		annDisplay = false;
 	}
 })
